a19fbb058e86fb6cde4a5ed81f7b487bd27af13a
Jina Park <pja0707@yonsei.ac.kr>
2022-10-18 07:40:02 +0000

    version 1.1.0

--- src/lsirm1pl_mcar_ss.cpp
+++ src/lsirm1pl_mcar_ss.cpp
@@ -4,43 +4,43 @@
 #include <RcppArmadillo.h>
 using namespace arma;
 // [[Rcpp::export]]
-Rcpp::List lsirm1pl_mcar_ss_cpp(arma::mat data, const int ndim, const int niter, const int nburn, const int nthin, const int nprint, 
+Rcpp::List lsirm1pl_mcar_ss_cpp(arma::mat data, const int ndim, const int niter, const int nburn, const int nthin, const int nprint,
                              const double jump_beta, const double jump_theta, const double jump_gamma, const double jump_z, const double jump_w,
                              const double pr_mean_beta, const double pr_sd_beta, const double pr_mean_theta,
                              double pr_spike_mean, const double pr_spike_sd, const double pr_slab_mean, const double pr_slab_sd,
                              const double pr_a_theta, const double pr_b_theta, const double pr_beta_a, const double pr_beta_b, const double missing){
-  
+
   int i, j, k, count, accept;
   double num, den, old_like_beta, new_like_beta, old_like_theta, new_like_theta, pr_sd_theta = 1.0;
   double old_like_z, new_like_z, old_like_w, new_like_w, old_like_gamma, new_like_gamma ;
   double ratio, un, post_a, post_b, dist_temp, dist_old_temp, dist_new_temp;
   double pr_mean_z = 0.0, pr_sd_z = 1.0, pr_mean_w = 0.0, pr_sd_w = 1.0, mle;
-  
+
   const int nsample = data.n_rows;
   const int nitem = data.n_cols;
-  
+
   arma::dvec oldbeta(nitem, fill::randu);
   oldbeta = oldbeta * 4.0 - 2.0;
   arma::dvec newbeta = oldbeta;
-  
+
   arma::dvec oldtheta(nsample, fill::randu);
   oldtheta = oldtheta * 4.0 - 2.0;
   arma::dvec newtheta = oldtheta;
-  
+
   arma::dmat oldz(nsample,ndim,fill::randu);
   oldz = oldz * 2.0 - 1.0;
   arma::dmat newz = oldz;
-  
+
   arma::dmat oldw(nitem,ndim,fill::randu);
   oldw = oldw * 2.0 - 1.0;
   arma::dmat neww = oldw;
-  
+
   double oldgamma = 1.0, newgamma = 1.0, pi_prob = 0.5, xi;
-  int pi; 
-  
+  int pi;
+
   pi = R::rbinom(1, pi_prob);
   xi = R::rbeta(pr_beta_a + pi, pr_beta_b + 1.0 - pi);
-  
+
   arma::dmat samp_beta((niter-nburn)/nthin, nitem, fill::zeros);
   arma::dmat samp_theta((niter-nburn)/nthin, nsample, fill::zeros);
   arma::dcube samp_z(((niter-nburn)/nthin), nsample, ndim, fill::zeros);
@@ -48,25 +48,25 @@ Rcpp::List lsirm1pl_mcar_ss_cpp(arma::mat data, const int ndim, const int niter,
   arma::dvec samp_sd_theta((niter-nburn)/nthin, fill::zeros);
   arma::dvec sample_mle((niter-nburn)/nthin, fill::zeros);
   arma::dvec samp_gamma(((niter-nburn)/nthin), fill::zeros);
-  arma::ivec samp_pi(((niter-nburn)/nthin), fill::zeros);
+  arma::dvec samp_pi(((niter-nburn)/nthin), fill::zeros);
   arma::dvec samp_xi(((niter-nburn)/nthin), fill::zeros);
-  
+
   arma::dvec accept_beta(nitem, fill::zeros);
   arma::dvec accept_theta(nsample, fill::zeros);
   arma::dvec accept_z(nsample, fill::zeros);
   arma::dvec accept_w(nitem, fill::zeros);
   double accept_gamma=0;
-  
+
   accept = count = 0;
-  
+
   arma::dmat dist(nsample,nitem,fill::zeros);
   arma::dvec old_dist_k(nitem,fill::zeros);
   arma::dvec new_dist_k(nitem,fill::zeros);
   arma::dvec old_dist_i(nsample,fill::zeros);
   arma::dvec new_dist_i(nsample,fill::zeros);
-  
+
   for(int iter = 0; iter < niter; iter++){
-    
+
     //dist(j,i) is distance of z_j and w_i
     dist.fill(0.0);
     for(i = 0; i < nitem; i++){
@@ -76,7 +76,7 @@ Rcpp::List lsirm1pl_mcar_ss_cpp(arma::mat data, const int ndim, const int niter,
         dist(k,i) = std::sqrt(dist_temp);
       }
     }
-    
+
     // beta update
     for(i = 0; i < nitem; i++){
       newbeta(i) = R::rnorm(oldbeta(i), jump_beta);
@@ -89,31 +89,31 @@ Rcpp::List lsirm1pl_mcar_ss_cpp(arma::mat data, const int ndim, const int niter,
           else old_like_beta += -std::log(1.0 + std::exp(oldbeta(i) + oldtheta(k) - oldgamma * dist(k,i)));
         }
       }
-      
+
       num = new_like_beta + R::dnorm4(newbeta(i), pr_mean_beta, pr_sd_beta, 1);
       den = old_like_beta + R::dnorm4(oldbeta(i), pr_mean_beta, pr_sd_beta, 1);
       ratio = num - den;
-      
+
       if(ratio > 0.0) accept = 1;
       else{
         un = R::runif(0,1);
         if(std::log(un) < ratio) accept = 1;
         else accept = 0;
       }
-      
+
       if(accept == 1){
         oldbeta(i) = newbeta(i);
         accept_beta(i) += 1.0 / (niter * 1.0);
       }
       else newbeta(i) = oldbeta(i);
-      
+
     }
-    
+
     // theta update
     for(k = 0; k < nsample; k++){
       newtheta(k) = R::rnorm( oldtheta(k), jump_theta);
       old_like_theta = new_like_theta = 0.0;
-      
+
       for(i = 0; i < nitem; i++){
         if(data(k,i) != missing){
           if(data(k,i) == 1.0) new_like_theta += -std::log(1.0 + std::exp(-(oldbeta(i) + newtheta(k) - oldgamma * dist(k,i))));
@@ -125,25 +125,25 @@ Rcpp::List lsirm1pl_mcar_ss_cpp(arma::mat data, const int ndim, const int niter,
       num = new_like_theta + R::dnorm4(newtheta(k), pr_mean_theta, pr_sd_theta, 1);
       den = old_like_theta + R::dnorm4(oldtheta(k), pr_mean_theta, pr_sd_theta, 1);
       ratio = num - den;
-      
+
       if(ratio > 0.0) accept = 1;
       else{
         un = R::runif(0,1);
         if(std::log(un) < ratio) accept = 1;
         else accept = 0;
       }
-      
+
       if(accept == 1){
         oldtheta(k) = newtheta(k);
         accept_theta(k) += 1.0 / (niter * 1.0);
       }
       else newtheta(k) = oldtheta(k);
     }
-    
+
     // gamma(log(gamma)) update
     newgamma = R::rlnorm(std::log(oldgamma), jump_gamma);
     old_like_gamma = new_like_gamma = 0.0;
-    
+
     for(k = 0; k < nsample; k++){
       for(i = 0; i < nitem; i++){
         if(data(k,i) != missing){
@@ -154,48 +154,48 @@ Rcpp::List lsirm1pl_mcar_ss_cpp(arma::mat data, const int ndim, const int niter,
         }
       }
     }
-    
+
     num = new_like_gamma + R::dlnorm(oldgamma, std::log(newgamma), jump_gamma, 1) ;
     den = old_like_gamma + R::dlnorm(newgamma, std::log(oldgamma), jump_gamma, 1) ;
     if(pi == 1){
       num += R::dlnorm(newgamma, pr_slab_mean, pr_slab_sd, 1);
       den += R::dlnorm(oldgamma, pr_slab_mean, pr_slab_sd, 1);
-    } 
+    }
     else{
       num += R::dlnorm(newgamma, pr_spike_mean, pr_spike_sd, 1);
       den += R::dlnorm(oldgamma, pr_spike_mean, pr_spike_sd, 1);
-    } 
-    
+    }
+
     ratio = num - den;
-    
+
     if(ratio > 0.0) accept = 1;
     else{
       un = R::runif(0,1);
       if(std::log(un) < ratio) accept = 1;
       else accept = 0;
     }
-    
+
     if(accept == 1){
       oldgamma = newgamma;
       accept_gamma += 1.0 / (niter * 1.0);
     }
     else newgamma = oldgamma;
-    
-    
+
+
     //pi update(spike slab parameter)
     pi_prob  = xi * R::dlnorm(oldgamma, pr_slab_mean, pr_slab_sd, 0);
     pi_prob /= (1.0 - xi) * R::dlnorm(oldgamma, pr_spike_mean, pr_spike_sd, 0) + xi * R::dlnorm(oldgamma, pr_slab_mean, pr_slab_sd, 0);
     pi = R::rbinom(1, pi_prob);
-    
+
     // xi update(spike slab parameter)
     xi = R::rbeta(pr_beta_a + pi, pr_beta_b + 1.0 - pi);
-    
-    
+
+
     // zj update
     for(k = 0; k < nsample; k++){
       for(j = 0; j < ndim; j++) newz(k,j) = R::rnorm(oldz(k,j), jump_z);
       old_like_z = new_like_z = 0.0;
-      
+
       //calculate distance of oldw and newz
       for(i = 0; i < nitem; i++){
         dist_old_temp = dist_new_temp = 0.0;
@@ -206,7 +206,7 @@ Rcpp::List lsirm1pl_mcar_ss_cpp(arma::mat data, const int ndim, const int niter,
         new_dist_k(i) = sqrt(dist_new_temp);
         old_dist_k(i) = sqrt(dist_old_temp);
       }
-      
+
       //calculate likelihood
       for(i = 0; i < nitem; i++){
         if(data(k,i) != missing){
@@ -216,24 +216,24 @@ Rcpp::List lsirm1pl_mcar_ss_cpp(arma::mat data, const int ndim, const int niter,
           else old_like_z += -std::log(1.0 + std::exp(oldbeta(i) + oldtheta(k) - oldgamma * old_dist_k(i)));
         }
       }
-      
+
       num = den = 0.0;
       for(j = 0; j < ndim; j++){
         num += R::dnorm4(newz(k,j),pr_mean_z,pr_sd_z,1);
         den += R::dnorm4(oldz(k,j),pr_mean_z,pr_sd_z,1);
       }
-      
+
       num += new_like_z;
       den += old_like_z;
       ratio = num - den;
-      
+
       if(ratio > 0.0) accept = 1;
       else{
         un = R::runif(0,1);
         if(std::log(un) < ratio) accept = 1;
         else accept = 0;
       }
-      
+
       if(accept == 1){
         for(j = 0; j < ndim; j++) oldz(k,j) = newz(k,j);
         accept_z(k) += 1.0 / (niter * 1.0);
@@ -242,12 +242,12 @@ Rcpp::List lsirm1pl_mcar_ss_cpp(arma::mat data, const int ndim, const int niter,
         for(j = 0; j < ndim; j++) newz(k,j) = oldz(k,j);
       }
     }
-    
+
     // wi update
     for(i = 0; i < nitem; i++){
       for(j = 0; j < ndim; j++) neww(i,j) = R::rnorm(oldw(i,j), jump_w);
       old_like_w = new_like_w = 0.0;
-      
+
       //calculate distance of neww and oldz
       for(k = 0; k < nsample; k++){
         dist_old_temp = dist_new_temp = 0.0;
@@ -258,7 +258,7 @@ Rcpp::List lsirm1pl_mcar_ss_cpp(arma::mat data, const int ndim, const int niter,
         new_dist_i(k) = sqrt(dist_new_temp);
         old_dist_i(k) = sqrt(dist_old_temp);
       }
-      
+
       //calculate likelihood
       for(k = 0; k < nsample; k++){
         if(data(k,i) != missing){
@@ -268,40 +268,40 @@ Rcpp::List lsirm1pl_mcar_ss_cpp(arma::mat data, const int ndim, const int niter,
           else old_like_w += -std::log(1.0 + std::exp(oldbeta(i) + oldtheta(k) - oldgamma * old_dist_i(k)));
         }
       }
-      
+
       num = den = 0.0;
       for(j = 0; j < ndim; j++){
         num += R::dnorm4(neww(i,j),pr_mean_w,pr_sd_w,1);
         den += R::dnorm4(oldw(i,j),pr_mean_w,pr_sd_w,1);
       }
-      
+
       num += new_like_w;
       den += old_like_w;
       ratio = num - den;
-      
+
       if(ratio > 0.0) accept = 1;
       else{
         un = R::runif(0,1);
         if(std::log(un) < ratio) accept = 1;
         else accept = 0;
       }
-      
+
       if(accept == 1){
         for(j = 0; j < ndim; j++) oldw(i,j) = neww(i,j);
         accept_w(i) += 1.0 / (niter * 1.0);
       }
       else{
         for(j = 0; j < ndim; j++) neww(i,j) = oldw(i,j);
-      } 
+      }
     }
-    
+
     //sigma_theta update with gibbs
     post_a = 2 * pr_a_theta  + nsample;
     post_b = pr_b_theta;
     for(j = 0; j < nsample; j++) post_b += std::pow((oldtheta(j) - pr_mean_theta), 2.0) / 2;
     pr_sd_theta = std::sqrt(2 * post_b *(1.0 /  R::rchisq(post_a)));
-    
-    //Burn in 
+
+    //Burn in
     if(iter >= nburn && iter % nthin == 0){
       for(i = 0; i < nitem; i++) samp_beta(count,i) = oldbeta(i);
       for(k = 0; k < nsample; k++) samp_theta(count,k) = oldtheta(k);
@@ -315,12 +315,12 @@ Rcpp::List lsirm1pl_mcar_ss_cpp(arma::mat data, const int ndim, const int niter,
           samp_z(count,k,j) = oldz(k,j);
         }
       }
-      
+
       samp_gamma(count) = oldgamma;
       samp_pi(count) = pi;
       samp_xi(count) = xi;
       samp_sd_theta(count) = pr_sd_theta;
-      
+
       //dist(j,i) is distance of z_j and w_i
       dist.fill(0.0);
       for(i = 0; i < nitem; i++){
@@ -330,7 +330,7 @@ Rcpp::List lsirm1pl_mcar_ss_cpp(arma::mat data, const int ndim, const int niter,
           dist(k,i) = std::sqrt(dist_temp);
         }
       }
-      
+
       mle = 0.0;
       for(i = 0; i < nitem; i++) mle += R::dnorm4(oldbeta(i), pr_mean_beta, pr_sd_beta, 1);
       for(k = 0; k < nsample; k++) mle += R::dnorm4(oldtheta(k), pr_mean_theta, pr_sd_theta, 1);
@@ -351,18 +351,18 @@ Rcpp::List lsirm1pl_mcar_ss_cpp(arma::mat data, const int ndim, const int niter,
       sample_mle(count) = mle;
       count++;
     } // burn, thin
-    
+
     if(iter % nprint == 0){
-      Rprintf("Iteration: %.5u ", iter); 
+      Rprintf("Iteration: %.5u ", iter);
       for(i = 0 ; i < nitem ; i++ ) {
         Rprintf("% .3f ", oldbeta(i));
       }
       Rprintf(" %.3f ", oldgamma);
       Rprintf(" %.3f\n", pr_sd_theta);
     }
-    
+
   } //for end
-  
+
   Rcpp::List output;
   output["beta"] = samp_beta;
   output["theta"] = samp_theta;
@@ -378,13 +378,13 @@ Rcpp::List lsirm1pl_mcar_ss_cpp(arma::mat data, const int ndim, const int niter,
   output["accept_z"] = accept_z;
   output["accept_w"] = accept_w;
   output["accept_gamma"] = accept_gamma;
-  
+
   return(output);
-  
+
 } // function end
 
 
 // You can include R code blocks in C++ files processed with sourceCpp
-// (useful for testing and development). The R code will be automatically 
+// (useful for testing and development). The R code will be automatically
 // run after the compilation.
 
--- src/lsirm1pl_normal_mcar_ss.cpp
+++ src/lsirm1pl_normal_mcar_ss.cpp
@@ -5,7 +5,7 @@
 using namespace arma;
 
 // This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp 
+// source this function into an R session using the Rcpp::sourceCpp
 // function (or via the Source button on the editor toolbar). Learn
 // more about Rcpp at:
 //
@@ -17,10 +17,10 @@ using namespace arma;
 Rcpp::List lsirm1pl_normal_mcar_ss_cpp(arma::mat data, const int ndim, const int niter, const int nburn, const int nthin, const int nprint,
                               const double jump_beta, const double jump_theta, const double jump_gamma, const double jump_z, const double jump_w,
                               const double pr_mean_beta, const double pr_sd_beta, const double pr_mean_theta,
-                              const double pr_spike_mean, const double pr_spike_sd, const double pr_slab_mean, const double pr_slab_sd, 
+                              const double pr_spike_mean, const double pr_spike_sd, const double pr_slab_mean, const double pr_slab_sd,
                               const double pr_a_theta, const double pr_b_theta, const double pr_a_eps, const double pr_b_eps,
                               const double pr_beta_a, const double pr_beta_b, const double missing){
-  
+
   const int nsample = data.n_rows;
   const int nitem = data.n_cols;
 
@@ -35,25 +35,25 @@ Rcpp::List lsirm1pl_normal_mcar_ss_cpp(arma::mat data, const int ndim, const int
   arma::dvec oldbeta(nitem, fill::randu);
   oldbeta = oldbeta * 4.0 - 2.0;
   arma::dvec newbeta = oldbeta;
-  
+
   arma::dvec oldtheta(nsample, fill::randu);
   oldtheta = oldtheta * 4.0 - 2.0;
   arma::dvec newtheta = oldtheta;
-  
+
   arma::dmat oldz(nsample,ndim,fill::randu);
   oldz = oldz * 2.0 - 1.0;
   arma::dmat newz = oldz;
-  
+
   arma::dmat oldw(nitem,ndim,fill::randu);
   oldw = oldw * 2.0 - 1.0;
   arma::dmat neww = oldw;
-  
-  
+
+
   double oldgamma = 1, newgamma = 1 , pi_prob = 0.5, xi;
   int pi;
   pi = R::rbinom(1, pi_prob);
   xi = R::rbeta(pr_beta_a + pi, pr_beta_b + 1.0 - pi);
-  
+
   arma::dmat samp_beta((niter-nburn)/nthin, nitem, fill::zeros);
   arma::dmat samp_theta((niter-nburn)/nthin, nsample, fill::zeros);
   arma::dcube samp_z(((niter-nburn)/nthin), nsample, ndim, fill::zeros);
@@ -64,15 +64,15 @@ Rcpp::List lsirm1pl_normal_mcar_ss_cpp(arma::mat data, const int ndim, const int
   arma::dvec samp_gamma(((niter-nburn)/nthin), fill::zeros);
   arma::ivec samp_pi(((niter-nburn)/nthin), fill::zeros);
   arma::dvec samp_xi(((niter-nburn)/nthin), fill::zeros);
-  
+
   arma::dvec accept_beta(nitem, fill::zeros);
   arma::dvec accept_theta(nsample, fill::zeros);
   arma::dvec accept_z(nsample, fill::zeros);
   arma::dvec accept_w(nitem, fill::zeros);
   double accept_gamma=0;
-  
+
   accept = count = 0;
-  
+
   arma::dmat dist(nsample,nitem,fill::zeros);
   arma::dvec old_dist_k(nitem,fill::zeros);
   arma::dvec new_dist_k(nitem,fill::zeros);
@@ -80,7 +80,7 @@ Rcpp::List lsirm1pl_normal_mcar_ss_cpp(arma::mat data, const int ndim, const int
   arma::dvec new_dist_i(nsample,fill::zeros);
 
   for(int iter = 0; iter < niter; iter++){
-    
+
     //dist(j,i) is distance of z_j and w_i
     dist.fill(0.0);
     for(i = 0; i < nitem; i++){
@@ -90,7 +90,7 @@ Rcpp::List lsirm1pl_normal_mcar_ss_cpp(arma::mat data, const int ndim, const int
         dist(k,i) = std::sqrt(dist_temp);
       }
     }
-    
+
     // beta update
     for(i = 0; i < nitem; i++){
       newbeta(i) = R::rnorm(oldbeta(i), jump_beta);
@@ -101,31 +101,31 @@ Rcpp::List lsirm1pl_normal_mcar_ss_cpp(arma::mat data, const int ndim, const int
           old_like_beta += - pow((data(k,i) - oldbeta(i) - oldtheta(k) + oldgamma * dist(k,i)), 2) / (2 * pow(pr_sd, 2)) ;
         }
       }
-      
+
       num = new_like_beta + R::dnorm4(newbeta(i), pr_mean_beta, pr_sd_beta, 1);
       den = old_like_beta + R::dnorm4(oldbeta(i), pr_mean_beta, pr_sd_beta, 1);
       ratio = num - den;
-      
+
       if(ratio > 0.0) accept = 1;
       else{
         un = R::runif(0,1);
         if(std::log(un) < ratio) accept = 1;
         else accept = 0;
       }
-      
+
       if(accept == 1){
         oldbeta(i) = newbeta(i);
         accept_beta(i) += 1.0 / (niter * 1.0);
       }
       else newbeta(i) = oldbeta(i);
-      
+
     }
-    
+
     // theta update
     for(k = 0; k < nsample; k++){
       newtheta(k) = R::rnorm( oldtheta(k), jump_theta);
       old_like_theta = new_like_theta = 0.0;
-      
+
       for(i = 0; i < nitem; i++){
         if(data(k,i) != missing){
         new_like_theta += - pow((data(k,i) - oldbeta(i) - newtheta(k) + oldgamma * dist(k,i)),2) / (2 * pow(pr_sd, 2)) ;
@@ -135,25 +135,25 @@ Rcpp::List lsirm1pl_normal_mcar_ss_cpp(arma::mat data, const int ndim, const int
       num = new_like_theta + R::dnorm4(newtheta(k), pr_mean_theta, pr_sd_theta, 1);
       den = old_like_theta + R::dnorm4(oldtheta(k), pr_mean_theta, pr_sd_theta, 1);
       ratio = num - den;
-      
+
       if(ratio > 0.0) accept = 1;
       else{
         un = R::runif(0,1);
         if(std::log(un) < ratio) accept = 1;
         else accept = 0;
       }
-      
+
       if(accept == 1){
         oldtheta(k) = newtheta(k);
         accept_theta(k) += 1.0 / (niter * 1.0);
       }
       else newtheta(k) = oldtheta(k);
     }
-    
+
     // gamma(log(gamma)) update
     newgamma = R::rlnorm(std::log(oldgamma), jump_gamma);
     old_like_gamma = new_like_gamma = 0.0;
-    
+
     for(k = 0; k < nsample; k++){
       for(i = 0; i < nitem; i++){
         if(data(k,i) != missing){
@@ -162,46 +162,46 @@ Rcpp::List lsirm1pl_normal_mcar_ss_cpp(arma::mat data, const int ndim, const int
         }
       }
     }
-    
+
     num = new_like_gamma + R::dlnorm(oldgamma, std::log(newgamma), jump_gamma, 1) ;
     den = old_like_gamma + R::dlnorm(newgamma, std::log(oldgamma), jump_gamma, 1) ;
     if(pi == 1){
       num += R::dlnorm(newgamma, pr_slab_mean, pr_slab_sd, 1);
       den += R::dlnorm(oldgamma, pr_slab_mean, pr_slab_sd, 1);
-    } 
+    }
     else{
       num += R::dlnorm(newgamma, pr_spike_mean, pr_spike_sd, 1);
       den += R::dlnorm(oldgamma, pr_spike_mean, pr_spike_sd, 1);
-    } 
-    
+    }
+
     ratio = num - den;
-    
+
     if(ratio > 0.0) accept = 1;
     else{
       un = R::runif(0,1);
       if(std::log(un) < ratio) accept = 1;
       else accept = 0;
     }
-    
+
     if(accept == 1){
       oldgamma = newgamma;
       accept_gamma += 1.0 / (niter * 1.0);
     }
     else newgamma = oldgamma;
-    
+
     //pi update(spike slab parameter)
     pi_prob  = xi * R::dlnorm(oldgamma, pr_slab_mean, pr_slab_sd, 0);
     pi_prob /= (1.0 - xi) * R::dlnorm(oldgamma, pr_spike_mean, pr_spike_sd, 0) + xi * R::dlnorm(oldgamma, pr_slab_mean, pr_slab_sd, 0);
     pi = R::rbinom(1, pi_prob);
-    
+
     // xi update(spike slab parameter)
     xi = R::rbeta(pr_beta_a + pi, pr_beta_b + 1.0 - pi);
-    
+
     // zj update
     for(k = 0; k < nsample; k++){
       for(j = 0; j < ndim; j++) newz(k,j) = R::rnorm(oldz(k,j), jump_z);
       old_like_z = new_like_z = 0.0;
-      
+
       //calculate distance of oldw and newz
       for(i = 0; i < nitem; i++){
         dist_old_temp = dist_new_temp = 0.0;
@@ -212,7 +212,7 @@ Rcpp::List lsirm1pl_normal_mcar_ss_cpp(arma::mat data, const int ndim, const int
         new_dist_k(i) = sqrt(dist_new_temp);
         old_dist_k(i) = sqrt(dist_old_temp);
       }
-      
+
       //calculate likelihood
       for(i = 0; i < nitem; i++){
         if(data(k,i) != missing){
@@ -220,7 +220,7 @@ Rcpp::List lsirm1pl_normal_mcar_ss_cpp(arma::mat data, const int ndim, const int
         old_like_z += - pow((data(k,i) - oldbeta(i) - oldtheta(k) + oldgamma * old_dist_k(i)),2) / (2 * pow(pr_sd, 2)) ;
         }
       }
-      
+
       num = den = 0.0;
       for(j = 0; j < ndim; j++){
         num += R::dnorm4(newz(k,j),pr_mean_z,pr_sd_z,1);
@@ -229,18 +229,18 @@ Rcpp::List lsirm1pl_normal_mcar_ss_cpp(arma::mat data, const int ndim, const int
       //Rprintf("%.3f %.3f %.3f %.3f\n", num, den, new_like_z, old_like_z);
       //arma::dvec newzz = dmvnorm(newz.cols(2*j,2*j+1),pr_mean_z,pr_cov_z,TRUE);
       //arma::dvec oldzz = dmvnorm(oldz.cols(2*j,2*j+1),pr_mean_z,pr_cov_z,TRUE);
-      
+
       num += new_like_z;
       den += old_like_z;
       ratio = num - den;
-      
+
       if(ratio > 0.0) accept = 1;
       else{
         un = R::runif(0,1);
         if(std::log(un) < ratio) accept = 1;
         else accept = 0;
       }
-      
+
       if(accept == 1){
         for(j = 0; j < ndim; j++) oldz(k,j) = newz(k,j);
         accept_z(k) += 1.0 / (niter * 1.0);
@@ -249,12 +249,12 @@ Rcpp::List lsirm1pl_normal_mcar_ss_cpp(arma::mat data, const int ndim, const int
         for(j = 0; j < ndim; j++) newz(k,j) = oldz(k,j);
       }
     }
-    
+
     // wi update
     for(i = 0; i < nitem; i++){
       for(j = 0; j < ndim; j++) neww(i,j) = R::rnorm(oldw(i,j), jump_w);
       old_like_w = new_like_w = 0.0;
-      
+
       //calculate distance of neww and oldz
       for(k = 0; k < nsample; k++){
         dist_old_temp = dist_new_temp = 0.0;
@@ -265,7 +265,7 @@ Rcpp::List lsirm1pl_normal_mcar_ss_cpp(arma::mat data, const int ndim, const int
         new_dist_i(k) = sqrt(dist_new_temp);
         old_dist_i(k) = sqrt(dist_old_temp);
       }
-      
+
       //calculate likelihood
       for(k = 0; k < nsample; k++){
         if(data(k,i) != missing){
@@ -273,31 +273,31 @@ Rcpp::List lsirm1pl_normal_mcar_ss_cpp(arma::mat data, const int ndim, const int
         old_like_w += - pow((data(k,i) - oldbeta(i) - oldtheta(k) + oldgamma * old_dist_i(k)),2) / (2 * pow(pr_sd, 2)) ;
         }
       }
-      
+
       num = den = 0.0;
       for(j = 0; j < ndim; j++){
         num += R::dnorm4(neww(i,j),pr_mean_w,pr_sd_w,1);
         den += R::dnorm4(oldw(i,j),pr_mean_w,pr_sd_w,1);
       }
-      
+
       num += new_like_w;
       den += old_like_w;
       ratio = num - den;
-      
+
       if(ratio > 0.0) accept = 1;
       else{
         un = R::runif(0,1);
         if(std::log(un) < ratio) accept = 1;
         else accept = 0;
       }
-      
+
       if(accept == 1){
         for(j = 0; j < ndim; j++) oldw(i,j) = neww(i,j);
         accept_w(i) += 1.0 / (niter * 1.0);
       }
       else{
         for(j = 0; j < ndim; j++) neww(i,j) = oldw(i,j);
-      } 
+      }
     }
 
 
@@ -352,11 +352,13 @@ Rcpp::List lsirm1pl_normal_mcar_ss_cpp(arma::mat data, const int ndim, const int
           samp_z(count,k,j) = oldz(k,j);
         }
       }
-      
+
       samp_gamma(count) = oldgamma;
+      samp_pi(count) = pi;
+      samp_xi(count) = xi;
       samp_sd_theta(count) = pr_sd_theta;
       samp_sd(count) = pr_sd;
-      
+
       mle = 0.0;
       for(i = 0; i < nitem; i++) mle += R::dnorm4(oldbeta(i), pr_mean_beta, pr_sd_beta, 1);
       for(k = 0; k < nsample; k++) mle += R::dnorm4(oldtheta(k), pr_mean_theta, pr_sd_theta, 1);
@@ -367,29 +369,29 @@ Rcpp::List lsirm1pl_normal_mcar_ss_cpp(arma::mat data, const int ndim, const int
       for(k = 0; k < nsample; k++){
         for(i = 0; i < nitem; i++){
           if(data(k,i) != missing){
-          mle += - pow((data(k,i) - oldbeta(i) - oldtheta(k) + oldgamma * dist(k,i)), 2) / (2 * pow(pr_sd, 2)) ; 
+          mle += - pow((data(k,i) - oldbeta(i) - oldtheta(k) + oldgamma * dist(k,i)), 2) / (2 * pow(pr_sd, 2)) ;
           }
         }
       }
       if(pi == 1) mle += R::dlnorm(oldgamma, pr_slab_mean, pr_slab_sd, 1);
       else mle += R::dnorm(oldgamma, pr_spike_mean, pr_spike_sd, 1);
-      
+
       samp_mle(count) = mle;
-      
+
       count++;
     }
-    
+
     if(iter % nprint == 0){
-      Rprintf("Iteration: %.5u ", iter); 
+      Rprintf("Iteration: %.5u ", iter);
       for(i = 0 ; i < nitem ; i++ ) {
         Rprintf("% .3f ", oldbeta(i));
       }
       Rprintf(" %.3f ", oldgamma);
       Rprintf(" %.3f\n", pr_sd_theta);
     }
-    
+
   } //for end
-  
+
   Rcpp::List output;
   output["beta"] = samp_beta;
   output["theta"] = samp_theta;
@@ -406,13 +408,13 @@ Rcpp::List lsirm1pl_normal_mcar_ss_cpp(arma::mat data, const int ndim, const int
   output["accept_z"] = accept_z;
   output["accept_w"] = accept_w;
   output["accept_gamma"] = accept_gamma;
-  
+
   return(output);
-  
+
 } // function end
 
 
 // You can include R code blocks in C++ files processed with sourceCpp
-// (useful for testing and development). The R code will be automatically 
+// (useful for testing and development). The R code will be automatically
 // run after the compilation.
 
